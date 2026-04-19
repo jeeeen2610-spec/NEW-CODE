9d1a696289a5e27e535527e58d49a33d33f26c45
jeeeen2610-spec <jeeeen2610@gmail.com>
2026-04-19 13:49:27 +0800

    ultralytics

--- __init__.py
+++ __init__.py
@@ -1,27 +1,7 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-__version__ = "8.2.0"
+from .rtdetr import RTDETR
+from .sam import SAM
+from .yolo import YOLO, YOLOWorld
 
-from ultralytics.data.explorer.explorer import Explorer
-from ultralytics.models import RTDETR, SAM, YOLO, YOLOWorld
-from ultralytics.models.fastsam import FastSAM
-from ultralytics.models.nas import NAS
-from ultralytics.utils import ASSETS, SETTINGS
-from ultralytics.utils.checks import check_yolo as checks
-from ultralytics.utils.downloads import download
-
-settings = SETTINGS
-__all__ = (
-    "__version__",
-    "ASSETS",
-    "YOLO",
-    "YOLOWorld",
-    "NAS",
-    "SAM",
-    "FastSAM",
-    "RTDETR",
-    "checks",
-    "download",
-    "settings",
-    "Explorer",
-)
+__all__ = "YOLO", "RTDETR", "SAM", "YOLOWorld"  # allow simpler import
